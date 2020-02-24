5cbe46f975956c8668d4e34054356b60fc37ff9b
Yurii Savchuk <yurii.savchuk@ukietech.com>
2020-02-24 22:21:00 +0200

    Added try/catch.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,17 +6,23 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
 
-    const searchApi = async () => {
-        const response = await yelp.get('/search', {
-            params: {
-                limit: 50,
-                term: term,
-                location: 'san jose'
-            }
-        }).catch(error => console.log('error=',error));
+    const searchApi = async (searchTerm) => {
+        try {
+            const response = await yelp.get('/people/1', {
+                params: {
+                    limit: 50,
+                    term: searchTerm,
+                    location: 'san jose'
+                }
+            }).catch(error => console.log('error=',error));
 
-        setResults(response.data.businesses);
+            setResults(response.data.businesses);
+        } catch (err) {
+            console.log('err=', err);
+            setErrorMessage('Something went wrong');
+        }
     };
 
     return (
@@ -24,10 +30,10 @@ const SearchScreen = () => {
             <SearchBar
                 term={term}
                 onTermChange={setTerm}
-                onTermSubmit={searchApi}
+                onTermSubmit={() => searchApi(term)}
             />
-            <Text>Search Screen {term}</Text>
-            <Text>We have found {results.length}</Text>
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
+            <Text>We have found: {results.length}</Text>
         </View>
     )
 };
