6eb03a531c3a244aea0179f865a03e20f7aff220
Yurii Savchuk <yurii.savchuk@ukietech.com>
2020-02-24 22:38:45 +0200

    Added Bearer =).

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "https://api.yelp.com/v3/businesses",
+    baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
         Authorization:
-            'MfhnBiGwBXPHTng50aQ-YrzM4hTy5OSVHPpoUDSvDLjEaPwjhW4WN_p4BfV9YECfP04KLZIEb8rhN8HtC-VKfSJd5MaH1zPy8OKdVh2w1Zirehv4HGKmS33QV41RXnYx'
+            'Bearer MfhnBiGwBXPHTng50aQ-YrzM4hTy5OSVHPpoUDSvDLjEaPwjhW4WN_p4BfV9YECfP04KLZIEb8rhN8HtC-VKfSJd5MaH1zPy8OKdVh2w1Zirehv4HGKmS33QV41RXnYx'
     }
 });
